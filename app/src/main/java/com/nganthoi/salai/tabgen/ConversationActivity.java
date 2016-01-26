e9534d519fb38830eed844f11d290edee2ce1dad
Nganthoiba <leecba@gmail.com>
2016-01-26 10:00:20 +0530

    ChantEngine design

--- app/src/main/java/com/nganthoi/salai/tabgen/ConversationActivity.java
+++ app/src/main/java/com/nganthoi/salai/tabgen/ConversationActivity.java
@@ -5,16 +5,35 @@ import android.os.Bundle;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+
+import java.text.DateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
+import chattingEngine.ChatAdapter;
+import chattingEngine.ChatMessage;
 
 public class ConversationActivity extends AppCompatActivity {
     ImageButton sendMessage;
     ImageView backButton,conv_Icon;
+    ListView messagesContainer;
+    EditText messageEditText;
+    ImageButton sendBtn;
+    private ChatAdapter adapter;
+    private ArrayList<ChatMessage> chatHistory;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -51,15 +70,16 @@ public class ConversationActivity extends AppCompatActivity {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });*/
-        sendMessage = (ImageButton) findViewById(R.id.sendMsg);
+        });
+        sendMessage = (ImageButton) findViewById(R.id.chatSendButton);
         sendMessage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Snackbar.make(v, "No action is implemented with this button", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        });*/
+        initControls();
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -82,5 +102,65 @@ public class ConversationActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+    private void initControls() {
+        messagesContainer = (ListView) findViewById(R.id.chatListView);
+        messageEditText = (EditText) findViewById(R.id.messageEditText);
+        sendBtn = (ImageButton) findViewById(R.id.chatSendButton);
+        loadDummyHistory();
 
+        sendBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String messageText = messageEditText.getText().toString();
+                if (TextUtils.isEmpty(messageText)) {
+                    return;
+                }
+
+                ChatMessage chatMessage = new ChatMessage();
+                chatMessage.setId(122);//dummy
+                chatMessage.setMessage(messageText);
+                chatMessage.setDate(DateFormat.getDateTimeInstance().format(new Date()));
+                chatMessage.setMe(true);
+                messageEditText.setText("");
+                displayMessage(chatMessage);
+            }
+        });
+    }
+
+    public void displayMessage(ChatMessage message) {
+        adapter.add(message);
+        adapter.notifyDataSetChanged();
+        scroll();
+    }
+
+    private void scroll() {
+        messagesContainer.setSelection(messagesContainer.getCount() - 1);
+    }
+
+    private void loadDummyHistory(){
+
+        chatHistory = new ArrayList<ChatMessage>();
+
+        ChatMessage msg = new ChatMessage();
+        msg.setId(1);
+        msg.setMe(false);
+        msg.setMessage("Hi");
+        msg.setDate(DateFormat.getDateTimeInstance().format(new Date()));
+        chatHistory.add(msg);
+        ChatMessage msg1 = new ChatMessage();
+        msg1.setId(2);
+        msg1.setMe(false);
+        msg1.setMessage("How r u doing???");
+        msg1.setDate(DateFormat.getDateTimeInstance().format(new Date()));
+        chatHistory.add(msg1);
+
+        adapter = new ChatAdapter(ConversationActivity.this, new ArrayList<ChatMessage>());
+        messagesContainer.setAdapter(adapter);
+
+        for(int i=0; i<chatHistory.size(); i++) {
+            ChatMessage message = chatHistory.get(i);
+            displayMessage(message);
+        }
+    }
 }
+
